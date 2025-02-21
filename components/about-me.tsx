7ec04c9d8ca98bac6aff9ad15b3d680b38f27de7
tzniceguy <joeshimbi@gmail.com>
2025-02-21 16:51:22 +0300

    about me section with cool aethtics

--- components/about-me.tsx
+++ components/about-me.tsx
@@ -6,18 +6,24 @@ import CodingHours from "./coding-hours";
 
 export default function About() {
   return (
-    <div className="grid md:grid-cols-2 gap-4 p-4  rounded-lg h-full">
-      <div className="flex flex-col gap-4">
-        <LocationCard />
-        <StacksCard />
-      </div>
-      <div className="flex flex-col gap-4">
-        <SocialLinksCard />
-        <div className="flex flex-col sm:flex-row gap-4 h-full">
-          <FavouriteStack />
-          <CodingHours />
+    <div className="h-full flex-flex-col items-center text-center">
+      <h1 className="text-3xl font-semibold">About Me</h1>
+      <div className="mt-10 grid md:grid-cols-2 gap-4 p-4  rounded-lg h-full">
+        <div className="flex flex-col gap-4">
+          <LocationCard />
+          <StacksCard />
+        </div>
+        <div className="flex flex-col gap-4">
+          <SocialLinksCard />
+          <div className="flex flex-col sm:flex-row gap-4 h-full">
+            <FavouriteStack />
+            <CodingHours />
+          </div>
         </div>
       </div>
+      <button className="mt-4 p-2 border rounded-md hover:bg-gray-100 dark:hover:bg-gray-800 transition-colors">
+        Know more about me
+      </button>
     </div>
   );
 }
