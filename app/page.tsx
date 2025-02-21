60971a5da3732d2a26503e7024a5e6223b9842b2
tzniceguy <joeshimbi@gmail.com>
2025-02-21 16:53:02 +0300

    homepage upgrade

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import About from "@/components/about-me";
 import Hero from "@/components/hero";
+import Showcase from "@/components/showcase";
 
 export default function Home() {
   return (
     <main>
       <Hero />
       <About />
+      <Showcase />
     </main>
   );
 }
