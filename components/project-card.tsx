4429457afeb6c90ce5685dc0ba66ffbaada0b8c0
tzniceguy <joeshimbi@gmail.com>
2025-03-01 12:25:09 +0300

    project card

--- components/project-card.tsx
+++ components/project-card.tsx
@@ -1,39 +1,55 @@
-import { FaLightbulb } from "react-icons/fa";
-import Image from "next/image";
 import Card from "./card";
+import Image from "next/image";
+import Link from "next/link";
 
-type CardProps = {
+interface CardProps {
   title: string;
   url: string;
   screenshot: string;
-};
-
-export default function SelectedProjects({
+  description: string;
+  stack?: string[];
+}
+export default function ProjectCard({
   title,
   url,
   screenshot,
+  description,
+  stack = [],
 }: CardProps) {
   return (
     <Card>
-      <div className="flex items-center gap-2">
-        <FaLightbulb color="#181717" size={20} />
-        <span className="font-medium"> {title}</span>
+      <div className="relative w-full h-52 rounded-md">
+        <Image
+          src={screenshot}
+          alt="project image"
+          fill={true}
+          className="rounded-md object-cover"
+        />
       </div>
-      <div className="mt-4">
-        <a
-          href={url}
-          target="_blank"
-          rel="noopener noreferrer"
-          className="block hover:opacity-80 transition-opacity"
-        >
-          <Image
-            src={screenshot} // Replace with actual image path
-            alt="Project Screenshot"
-            width={300}
-            height={200}
-            className="rounded-md object-cover"
-          />
-        </a>
+      <div className="flex flex-col gap-5">
+        <div className="flex items-center gap-2">
+          <Link
+            href={url}
+            target="_blank"
+            className="font-semibold text-lg hover:underline"
+          >
+            {title}
+          </Link>
+          <span className="w-2 h-2 bg-green-500 rounded-full animate-pulse" />
+        </div>
+        <div className="flex items-center gap-2">
+          <p className="text-sm text-gray-500">{description}</p>
+        </div>
+        <div className="flex gap-2">
+          {stack.map((tech) => (
+            <span
+              key={tech}
+              className="px-3 py-1 text-xs rounded-full bg-gray-100 font-medium "
+            >
+              {tech}
+            </span>
+          ))}
+        </div>
       </div>
     </Card>
   );
