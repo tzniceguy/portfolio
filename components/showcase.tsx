9d8c2df670690ea7142d5329d31bcf2b667d369b
tzniceguy <joeshimbi@gmail.com>
2025-03-01 12:27:07 +0300

    refactor: change the source of project to external source

--- components/showcase.tsx
+++ components/showcase.tsx
@@ -1,25 +1,13 @@
-import SelectedProjects from "./project-card";
-
-const projects = [
-  {
-    title: "Project One",
-    url: "https://github.com/yourusername/project-one",
-    screenshot: "/projects/project-one.jpg",
-  },
-  {
-    title: "Project Two",
-    url: "https://github.com/yourusername/project-two",
-    screenshot: "/projects/project-two.jpg",
-  },
-];
+import ProjectCard from "./selected-project-card";
+import { selectedProjects } from "@/lib/projects";
 
 export default function Showcase() {
   return (
     <div className="h-full flex-col items-center text-center mt-10">
       <h1 className="text-3xl font-semibold">Projects</h1>
       <div className="mt-10 grid md:grid-cols-2 gap-4 p-4  rounded-lg h-full">
-        {projects.map((project) => (
-          <SelectedProjects
+        {selectedProjects.map((project) => (
+          <ProjectCard
             key={project.title}
             title={project.title}
             url={project.url}
