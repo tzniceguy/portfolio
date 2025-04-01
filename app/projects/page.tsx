7a403505f7d5aefd091f556a7e79d72123e1b673
tzniceguy <joeshimbi@gmail.com>
2025-04-01 16:01:50 +0300

    refactor:making art in every writing

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -5,10 +5,15 @@ export default function Page() {
   return (
     <main className="max-w-4xl mx-auto">
       <div className="h-full flex-col mt-10">
-        <div className="border-b pb-6">
-          <h1 className="text-3xl font-semibold">projects</h1>
+        <div className="border-b pb-6 border-gray-200 ">
+          <h1 className="text-3xl font-semibold capitalize mb-4">projects</h1>
           <p className="mt-2 text-md">
-            the list of projects with my footprints
+            Welcome to my gallery of creations, where code meets curiosity.
+            These projects are more than just lines of logic, they’re
+            experiments, lessons, and late-night victories stitched together
+            with grit. From sleek frontends to robust backends, each one carries
+            a piece of my journey as a developer. Dive in, explore, and see what
+            I’ve been building when the ideas wouldn’t let me sleep.
           </p>
         </div>
         <div className="mt-10 grid md:grid-cols-2 gap-4 py-4 rounded-lg h-full">
