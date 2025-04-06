0860b03b2de76ab4f8cab1d074230e9ebae56ecc
tzniceguy <joeshimbi@gmail.com>
2025-04-06 20:14:27 +0300

    refactor: layout update

--- components/showcase.tsx
+++ components/showcase.tsx
@@ -3,9 +3,9 @@ import { selectedProjects } from "@/lib/projects";
 
 export default function Showcase() {
   return (
-    <div className="h-full flex-col items-center text-center mt-10">
+    <div className="h-full flex flex-col items-center text-center mt-10">
       <h1 className="text-3xl font-semibold">Projects</h1>
-      <div className="mt-10 grid md:grid-cols-2 gap-4 p-4  rounded-lg h-full">
+      <div className="mt-10 grid md:grid-cols-2 gap-4 p-4 rounded-lg h-full">
         {selectedProjects.map((project) => (
           <ProjectCard
             key={project.title}
