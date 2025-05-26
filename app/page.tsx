b0bd42bd0cc589dd0cb9d6f4e6aad63cbf03855a
joe <joeshimbi@gmail.com>
2025-05-26 10:45:04 +0300

    chore(home): update components names for import

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
 import About from "@/components/about-me";
 import Hero from "@/components/hero";
-import Showcase from "@/components/showcase";
+import Social from "@/components/social";
+import Contact from "@/components/contact";
 
 export default function Home() {
   return (
     <main className="max-w-4xl mx-auto">
       <Hero />
       <About />
-      <Showcase />
+      <Social />
+      <Contact />
     </main>
   );
 }
